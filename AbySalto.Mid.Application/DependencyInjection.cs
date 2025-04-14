f566d5574854f8a6a18f1f836d9aea01455c674a
Ajla Becic <ajlaa.be@gmail.com>
2025-04-14 03:45:29 +0200

    Add missing dependency

--- AbySalto.Mid.Application/DependencyInjection.cs
+++ AbySalto.Mid.Application/DependencyInjection.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 namespace AbySalto.Mid.Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
             return services;
         }
     }
